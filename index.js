7fa7903d7514d66e6d213239431c69f1ffabb65a
Terry Mitchelle <mitchellterry307@gmail.com>
2023-10-01 14:13:49 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let name = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(name,surprise){
+  let messages = [];
+  for(let i = 0;i<name.length;i++){
+  let message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift!`
+  messages.push(message);
+}
+  return messages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"],"surprise");
+function countDown(){
+  let countDown = 0;
+  while(countDown<11){
+      console.log(countDown++)
+  }
+}
